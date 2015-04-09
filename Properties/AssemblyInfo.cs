5d7eac42e1704da16b8a2adffdcc75cb8c9479d0
sebastian <sebastian@d252d4b6-3862-8246-84e1-cf8c128d3774>
2015-04-09 13:47:24 +0000

    Camelonta Umbraco Utilities - paket för hjälpmetoder

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Camelonta.Utilities")]
+[assembly: AssemblyProduct("Camelonta.Utilities")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
