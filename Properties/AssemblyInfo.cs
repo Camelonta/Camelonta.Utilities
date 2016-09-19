2ce5cba88b50c67cb6fea1c8ea3c99f25734af24
jesse <jesse@d252d4b6-3862-8246-84e1-cf8c128d3774>
2016-09-19 09:04:42 +0000

    camelonta utilities: - tog bort dependency UmbracoCMs. Måste kunna ha ett .Core-projekt med Utilities som inte laddar ned hela umbraco. Den ska egentligen dependa på Umbraco.Core

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.4.2")]
-[assembly: AssemblyFileVersion("1.1.4.2")]
+[assembly: AssemblyVersion("1.1.5")]
+[assembly: AssemblyFileVersion("1.1.5")]
